db29f1458a576c9675ce5e6218179405b6e9e0e8
Anderson Lomba <andersonlomba172@outlook.com>
2022-11-16 12:24:33 -0300

    Atualização do front

--- frontend/crudfrontend/src/UserList.js
+++ frontend/crudfrontend/src/UserList.js
@@ -4,6 +4,14 @@ import UserList from './UserList';
 
 
 export default class UserList extends React.Component(){
+    state = { lists: null}
+
+     async componentDiMount(){
+        var url = 'http://127.0.0.1:8000/list';
+        const response = await fetch(url);
+        const data = await response.json();
+     }
+
     render()
     {
     return (
