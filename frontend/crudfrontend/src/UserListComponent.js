a298f516fd02a5fada1384fe1f64e8cf08072d32
Anderson Lomba <andersonlomba172@outlook.com>
2022-11-16 03:32:10 -0300

    Criação de componentes

---
+++ frontend/crudfrontend/src/UserListComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function ListComponent(){
+    return (
+  <div>
+      <h2>Lista de usuários</h2>
+    <ul>
+        <li>Anderson Lomba</li>
+        <li>Josias Ferreira</li>
+
+    </ul>
+</div>
+    )
+}
--- frontend/crudfrontend/src/index.js
+++ frontend/crudfrontend/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UserListComponent from './UserListComponent';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <UserListComponent/>
   </React.StrictMode>
 );
 
